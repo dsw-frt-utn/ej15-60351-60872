0068db1bbd497084e8a3887f20c6e5c6aa160b80
CarlosSuarezLemoine <aar.suarez.lemoine@gmail.com>
2026-06-14 21:02:10 -0300

    Domain listo

---
+++ Dsw2026Ej15/Dsw2026Ej15.Domain/Speciality.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej15.Domain
+{
+    internal class Speciality : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Speciality(Guid id, string name, string description) : base (id)
+        {
+            Name = name;
+            Description = description;
+        }
+
+    }
+}

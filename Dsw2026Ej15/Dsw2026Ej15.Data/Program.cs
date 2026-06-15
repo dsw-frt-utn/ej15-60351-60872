ef9400cca96963638c6ec479faa4c7e84e5ae2f0
Dalmiro Ariel Ferreyra <DalmiroFerreyraalu.frt.utn.edu.ar>
2026-06-14 21:18:19 -0300

    Se desarrollo el proyecto Dsw2026_Ej15.Data

---
+++ Dsw2026Ej15/Dsw2026Ej15.Data/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Dsw2026Ej15
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApllication.CreateBuilder(args);
+
+        }
+    }
+}
